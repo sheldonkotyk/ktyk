409144a1fe726dfedbd54681672ec9b19e25dc84
Sheldon Kotyk <sheldon@kotyk.com>
2022-08-31 00:51:26 -0500

    podcaster

---
+++ resources/js/site.js
@@ -0,0 +1,86 @@
+import * as Turbo from "@hotwired/turbo"
+import Plyr from 'plyr';
+import Alpine from 'alpinejs';
+import chroma from "chroma-js";
+
+Alpine.store('ui', {
+    colors: chroma.scale([
+        window.primaryColor,
+        window.secondaryColor
+    ]).colors(5),
+});
+
+let plyr = new Plyr('audio', {
+    controls: ['play', 'rewind', 'progress', 'fast-forward', 'current-time', 'settings'],
+    iconUrl: '/img/plyr-sprite.svg',
+    seekTime: 15
+});
+
+Alpine.store('player', {
+    plyr: null,
+    playing: false,
+    audio_file: null,
+    episode_url: null,
+    title: null,
+    show: false,
+    init() {
+        plyr.on('playing', event => {
+            Alpine.store('player').playing = true
+        });
+        plyr.on('pause', event => {
+            Alpine.store('player').playing = false
+        });
+        plyr.on('ended', event => {
+            Alpine.store('player').playing = false
+        });
+    },
+    playEpisode(title, audio_file, mime_type, episode_url) {
+        if (this.audio_file !== audio_file) {
+            plyr.source = {
+                type: 'audio',
+                title: title,
+                audio_file: audio_file,
+                sources: [{ src: audio_file, type: mime_type }]
+            };
+        }
+        this.toggle(audio_file);
+        this.title = title;
+        this.audio_file = audio_file;
+        this.episode_url = episode_url;
+        this.show = true;
+    },
+    toggle(file) {
+        if (this.playing && file === this.audio_file) {
+            plyr.pause();
+
+            // Events not sent when calling pause programmatically
+            // after setting .source
+            Alpine.store('player').playing = false
+            return;
+        }
+        plyr.togglePlay()
+    },
+});
+
+Alpine.start();
+window.Alpine = Alpine;
+
+// Turbo + Alpine.js 3 bridge
+// via: https://gist.github.com/calebporzio/20cf74af4a015644c7bef5166cffd86c
+document.addEventListener('turbo:before-render', () => {
+    let permanents = document.querySelectorAll('[data-turbo-permanent]')
+
+    let undos = Array.from(permanents).map(el => {
+        el._x_ignore = true
+
+        return () => {
+            delete el._x_ignore
+        }
+    })
+
+    document.addEventListener('turbo:render', function handler() {
+        while(undos.length) undos.shift()()
+
+        document.removeEventListener('turbo:render', handler)
+    })
+})
