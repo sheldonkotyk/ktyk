d0092981716d5cf7464ee13c7149a8d984c25615
Sheldon Kotyk <sheldon@kotyk.com>
2023-11-24 22:14:58 -0600

    update commit msg to prevent deploys

--- config/statamic/git.php
+++ config/statamic/git.php
@@ -132,7 +132,7 @@ return [
 
     'commands' => [
         'git add {{ paths }}',
-        'git -c "user.name={{ name }}" -c "user.email={{ email }}" commit -m "{{ message }}"',
+        'git -c "user.name={{ name }}" -c "user.email={{ email }}" commit -m "{{ message }} [BOT]"',
     ],
 
     /*
